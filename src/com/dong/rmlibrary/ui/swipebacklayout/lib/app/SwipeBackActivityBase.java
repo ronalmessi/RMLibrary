dafc3e5d6564251cf01f0d6fec421318e36e2bcd
ronalmessi <ronaldinioh@163.com>
2014-12-09 08:13:34 +0800

    增加滑动返回组件

---
+++ src/com/dong/rmlibrary/ui/swipebacklayout/lib/app/SwipeBackActivityBase.java
@@ -0,0 +1,22 @@
+package com.dong.rmlibrary.ui.swipebacklayout.lib.app;
+
+import com.dong.rmlibrary.ui.swipebacklayout.SwipeBackLayout;
+
+
+/**
+ * @author Yrom
+ */
+public interface SwipeBackActivityBase {
+    /**
+     * @return the SwipeBackLayout associated with this activity.
+     */
+    public abstract SwipeBackLayout getSwipeBackLayout();
+
+    public abstract void setSwipeBackEnable(boolean enable);
+
+    /**
+     * Scroll out contentView and finish the activity
+     */
+    public abstract void scrollToFinishActivity();
+
+}
